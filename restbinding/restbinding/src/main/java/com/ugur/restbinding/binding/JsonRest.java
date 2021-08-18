0f3130f80433e8275c24467b89931c46bdb45548
Ugur hamzaoglu <craxx67@gmail.com>
2021-08-18 14:40:30 +0300

    Json.createObjectBuilder() usage

---
+++ restbinding/restbinding/src/main/java/com/ugur/restbinding/binding/JsonRest.java
@@ -0,0 +1,45 @@
+package com.ugur.restbinding.binding;
+
+import javax.json.Json;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+
+
+@Path("/json")
+public class JsonRest {
+	
+	
+	@GET
+	@Path("plain/{cat}/{id}")
+	@Produces("application/json; charset=UTF-8")
+	public String getJson(@PathParam("cat") String category ,@PathParam("id") long productId) {
+		return "{"+
+				"\"productId\":"+productId+","
+				+ "\"Category\" : \""+category+"\","
+				+"\"salesPrice\":1500.0"+
+				"}";
+	}
+	
+	// JSON create
+	@GET
+	@Path("processing")
+	@Produces("application/json; charset=UTF-8")
+	public String getProcessing() {
+		
+		return Json.createObjectBuilder()
+				.add("productId",501L)
+				.add("productName","Laptop")
+				.add("price",6700.0)
+				.build().toString();
+			
+		
+	}
+	
+	
+	
+	
+
+}

34ec8ac1c63431d740db159ca870a5e68265f4d3
Ugur hamzaoglu <craxx67@gmail.com>
2021-08-24 17:48:49 +0300

    settings changed

--- restsetup/src/main/java/com/ugur/restsetup/media/PlainRest.java
+++ restsetup/src/main/java/com/ugur/restsetup/media/PlainRest.java
@@ -3,7 +3,7 @@ package com.ugur.restsetup.media;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+
 
 @Path("/plain")
 public class PlainRest {
